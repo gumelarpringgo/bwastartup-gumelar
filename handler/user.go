98e7a2a7268b22bc66fa65e6253ff48ffcb56c88
gumelarpringgo <gumelarpringgo.media@gmail.com>
2022-11-24 10:56:06 +0700

    Init project. Register user  endpoint

---
+++ handler/user.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct di atas kita pasang sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "token")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
+func (h *userHandler) Login(c *gin.Context) {
+	// user memasukkan input (email dan pass)
+	// input ditangkat handler
+	// mapping dari input user ke input struct
+	// input struct passing service
+	// di service mencari dengan bantuan repository user dengan email
+	// mencocokkan password
+}
---
+++ user/entity.go
@@ -0,0 +1,22 @@
+package user
+
+import "time"
+
+type User struct {
+	ID             int
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+// NOTE
+//input
+//handler mpping input ke struct
+//service mapping ke struct User
+//repository save struct User ke db
+//db
