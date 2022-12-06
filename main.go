d3fb92db7e6b62f714d8a9d3ac78fb4b8baa300d
gumelarpringgo <gumelarpringgo.media@gmail.com>
2022-12-06 09:53:39 +0700

    Login endpoint

--- main.go
+++ main.go
@@ -19,12 +19,39 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+
+	// input := user.LoginInput{
+	// 	Email:    "gumelarpringgo.media@gmail.com",
+	// 	Password: "password",
+	// }
+	// user, err := userService.Login(input)
+
+	// if err != nil {
+	// 	fmt.Println("Terjadi kesalahan")
+	// 	fmt.Println(err.Error())
+	// }
+
+	// fmt.Println(user.Email)
+	// fmt.Println(user.Name)
+
+	// userByEmail, err := userRepository.FindByEmail("ggumelarpringgo.media@gmail.com")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// if userByEmail.ID == 0 {
+	// 	fmt.Println("User tidak ditemukan")
+	// } else {
+	// 	fmt.Println(userByEmail.Name)
+	// }
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run()
 
