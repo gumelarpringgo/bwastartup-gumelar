5dfbd2c027be8159d5d5520d9e549d3a3a4ee25c
gumelarpringgo <gumelarpringgo.media@gmail.com>
2022-12-13 10:09:07 +0700

    Campaign service : GetCampaigns

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,31 @@
 package campaign
+
+type Service interface {
+	FindCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	respository Repository
+}
+
+func NewService(respository Repository) *service {
+	return &service{respository}
+}
+
+func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+	if userID != 0 {
+		campaigns, err := s.respository.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
+	}
+
+	campaigns, err := s.respository.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
